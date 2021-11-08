1251e3dfbe218eca96f0ed4a266ea851657d8efb
Matt_Carter <matzcarter@gmail.com>
2021-11-08 11:13:52 -0500

    updating project

---
+++ js/index.js
@@ -0,0 +1,129 @@
+console.log("connected!")
+
+// ============================================//
+
+//Initializes the taskManager object
+let taskManager = new TaskManager();
+taskManager.load();
+taskManager.render();
+//Gets the task form element
+let taskForm = document.querySelector("#taskForm");
+
+
+//Function that validates input, and sets error messages
+function validFormFieldInput() {
+    let isValid = true;
+    let taskName = document.querySelector("input[name=name]").value;
+    let errorMessage = "";
+    if (taskName == "") {
+        errorMessage += "Task name cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskDescription = document.querySelector("input[name=description]").value;
+    if (taskDescription == "") {
+        errorMessage += "Task description cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskStatus = document.querySelector("#status").value;
+    if (taskStatus == "") {
+        errorMessage += "Task status cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskAssigned = document.querySelector("input[name=assignedTo").value;
+    if (taskAssigned == "") {
+        errorMessage += "Task assigned to cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskDate = document.querySelector("#tasktime").value;
+    if (taskDate == "") {
+        errorMessage += "Task date cannot be empty <br/>";
+        isValid = false;
+    }
+    let alert = document.querySelector("#alert");
+    alert.innerHTML = errorMessage;
+    return isValid;
+}
+
+
+
+
+//Sets a submit listener to task form, checks if valid to submit, or show error messages if not
+
+taskForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    //Checks if form is valid
+    let isValid = validFormFieldInput();
+
+    //Gets alert and success boxes (on top of form)
+    let alert = document.querySelector("#alert");
+    let success = document.querySelector("#success");
+    if (isValid == false) {
+        alert.classList.remove("d-none");
+        success.classList.add("d-none");
+        //Return and does not execute anything after this line.
+        return;
+    } else {
+        success.classList.remove("d-none");
+        alert.classList.add("d-none");
+    }
+
+    //Gets all form values
+    let taskName = document.querySelector("input[name=name]").value;
+    let taskDescription = document.querySelector("input[name=description]").value;
+    let taskStatus = document.querySelector("#status").value;
+    let taskAssigned = document.querySelector("input[name=assignedTo").value;
+    let taskDate = document.querySelector("#tasktime").value;
+
+    //Adds new task to task manager array
+    taskManager.addTask(taskName, taskDescription, taskAssigned, taskDate, taskStatus);
+    taskManager.save();
+    //Re-renders card list with new tasks
+    taskManager.render();
+
+
+    //Resets form
+    taskForm.reset();
+    
+    });
+
+// Select the Tasks List
+const tasksList = document.querySelector('#tasksList');
+
+// Add an 'onclick' event listener to the Tasks List
+tasksList.addEventListener('click', (event) => {
+    console.log(event);
+    // Check if a "Mark As Done" button was clicked
+    if (event.target.classList.contains('done-button')) {
+        // Get the parent Task.
+        const parentTask = event.target.parentElement.parentElement;
+
+        // Get the taskId of the parent Task.
+        const taskId = Number(parentTask.dataset.taskId);
+
+        // Get the task from the TaskManager using the taskId.
+        const task = taskManager.getTaskById(taskId);
+
+        if (task != null) {
+            // Update the task status to 'done'
+            task.status = 'Completed';
+            taskManager.save();
+        }
+        // Render the tasks
+        taskManager.render();
+
+    } else if (event.target.classList.contains('delete-button')) {
+        // Get the parent Task
+        const parentTask = event.target.parentElement.parentElement;
+
+        // Get the taskId of the parent Task.
+        const taskId = Number(parentTask.dataset.taskid);
+
+        taskManager.deleteTask(taskId);
+        taskManager.save();
+        // Render the tasks
+        taskManager.render();
+
+    }
+    
+})
---
+++ js/taskManager.js
@@ -0,0 +1,136 @@
+console.log("connected!")
+
+ //variable with the Cards
+ 
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status) =>{ 
+    let cardHtml = `<div class="card mx-auto" style="width: 18rem;" data-taskId="${id}">
+                                <div class="card-body">
+                                <h4 class="taskName">${name}</h4>
+                                <h6 class="taskDescription">${description}</h6>
+                                <h6 class="taskAssignedTo">${assignedTo}</h6>
+                                <h6 class="taskDueDate">${dueDate}</h6>
+                                <span class="taskStatus ${status == "Completed" ? "success" : ""}">${status}</span>`;
+                                
+    if (status != "Completed") {
+        cardHtml += `<button type="button" class="done-button btn btn-success">Done</button>`;
+    }
+    cardHtml +=
+                                `<button type="button" class="delete-button btn btn-danger">Delete</button>
+                                </div>
+                                 </div>`;
+
+
+    return cardHtml;
+
+    
+}
+
+
+
+class TaskManager {
+    //Constructor - Runs when using new
+    constructor(currentId = 0) {
+      this.tasks = [];
+      this.currentId = currentId;
+
+    }
+
+
+    //Method, adds a new task to this.tasks array
+    addTask( name, description, assignedTo, dueDate, status = "Todo"){
+        this.currentId += 1;
+        let task = {
+            id: this.currentId,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: status
+        }
+        this.tasks.push(task);
+    }
+
+    deleteTask(taskId){
+        const newTasks = [];
+        for(let i in this.tasks){
+            let task = this.tasks[i];
+            if (taskId != task.id) {
+                newTasks.push(task);
+            }
+        }
+
+        this.tasks = newTasks;
+
+    }
+
+    //Method, renders a card list using the tasks in the this.tasks array
+    render(){
+        let tasksHtmlList = [];
+
+        //Loops around the tasks array
+        for(let i = 0; i < this.tasks.length; i++){
+            let task = this.tasks[i];
+            //Gets date and formats it to readable format
+            let date = new Date(task.dueDate);
+            let formattedDate = date.toDateString();
+            let taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
+            //Adds task to html array
+            tasksHtmlList.push(taskHtml);
+        }
+
+        //Joins array to single html string
+        const tasksHtml = tasksHtmlList.join("\n");
+
+        //Adds string to main html
+        document.querySelector("#tasksList").innerHTML = tasksHtml;
+
+    }
+
+    getTaskById(id){
+        //Loop through tasks
+        for(let i in this.tasks){
+            //Get task at index i
+            const task = this.tasks[i];
+            //If task is equal to id
+            if (id == task.id){
+                //Return the task
+                return task ;
+            }
+        }
+        //Return null if no task with id found
+        return null;
+    }
+        //Save to the local storage with the JSON parse and will change all the array values into a readable format in the console.
+        save(){
+            const tasksJson = JSON.stringify(this.tasks);
+            localStorage.setItem("tasks", tasksJson);
+            const currentId = this.currentId.toString();
+            localStorage.setItem("currentId", currentId);
+        }
+
+        load(){
+            const tasksJson = localStorage.getItem("tasks");
+            if (tasksJson != null) {
+                this.tasks = JSON.parse(tasksJson);
+            }
+
+            const currentId = localStorage.getItem("currentId");
+            if (currentId != null) {
+                this.currentId = JSON.parse(currentId);
+            }
+        }
+        
+  
+  }
+
+
+        // So it can be included in testing
+        module.exports = TaskManager;
+
+
+  
+
+
+
+
+  
